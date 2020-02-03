804ec68130bfedecea996ff3f3ea7b431a46e967
obada <obadaquraan@yahoo.com>
2020-02-03 20:32:36 +0200

    initial-demo

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const basicAuth = require('./basic-auth-middleware.js');
+const users = require('./users.js');
+const oauth = require('./oauth-middleware.js');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+app.post('/signup', (req, res) => {
+  users.save(req.body)
+    .then(user => {
+      let token = users.generateToken(user);
+      res.status(200).send(token);
+    })
+});
+
+app.post('/signin', basicAuth, (req, res) => {
+  res.status(200).send(req.token);
+});
+
+app.get('/users', basicAuth, (req, res) => {
+  res.status(200).json(users.list());
+});
+
+app.get('/oauth', oauth, (req, res) => {
+  res.status(200).send(req.token);
+});
+
+app.listen(3000, () => console.log('server up'));
